6e9bc9402b4a91e4b4686a1681449888c24134da
Yonatan Reicher <yony252525@gmail.com>
2025-05-27 09:37:32 +0300

    Add Random.float32Between

--- src/Maths.Random.fs
+++ src/Maths.Random.fs
@@ -10,6 +10,10 @@ let float () : float = jsNative
 let float32 () = float32 (float ())
 
 
+let float32Between (min: float32) (max: float32) : float32 =
+    min + (max - min) * float32 ()
+
+
 let direction () =
     let angle = 2f<rad> * System.MathF.PI * float32 ()
     Vector.fromAngle angle
