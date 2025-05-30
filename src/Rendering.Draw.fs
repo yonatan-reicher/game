8b3c55b5942a371e92c03c08901cbf94e2f842e5
Yonatan Reicher <yony252525@gmail.com>
2025-05-30 11:46:24 +0300

    Adding units of measure to Draw type

--- src/Rendering.Draw.fs
+++ src/Rendering.Draw.fs
@@ -1,16 +1,17 @@
 namespace Rendering
 
 
+open Maths
 open Fable.Core
 
 
-type 'a Draw =
+type Draw<[<Measure>] 'u, 'a> =
     private
     | Draw of (CanvasContext -> 'a) // This does a side effect
 
     static member inline Return x = Draw(fun _ -> x)
 
-    static member inline (>>=)(x, f: 'a -> 'b Draw) =
+    static member inline (>>=)(x, f: 'a -> Draw<'u, 'b>) =
         let (Draw x) = x
 
         Draw(fun context ->
@@ -22,28 +23,37 @@ type 'a Draw =
         let (Draw action) = this
         action
 
-    static member FromAction(action: CanvasContext -> 'a) = Draw action
+    static member FromAction(action: CanvasContext -> 'a) : Draw<'u, 'a> = Draw action
 
 
 [<RequireQualifiedAccess>]
 module Draw =
-    let inline action (x: 'a Draw) = x.Action
+    let inline action (x: Draw<_, _>) = x.Action
 
-    let inline fromAction action : 'a Draw = Draw.FromAction action
+    let inline fromAction action = Draw.FromAction action
 
-    let width: float32 Draw = fromAction (fun context -> float32 context.canvas.width)
+    let size: Draw<px, Vec2<px>> =
+        fromAction (fun context ->
+            { X = float32 context.canvas.width * 1f<px>
+              Y = float32 context.canvas.height * 1f<px> })
 
-    let height: float32 Draw = fromAction (fun context -> float32 context.canvas.height)
+    let width: Draw<px, float32> =
+        fromAction (fun context -> float32 context.canvas.width)
+
+    let height: Draw<px, float32> =
+        fromAction (fun context -> float32 context.canvas.height)
 
     /// Clears the entire screen.
-    let clear: unit Draw =
+    let clear: Draw<px, unit> =
         fromAction (fun context ->
             let w = context.canvas.width
             let h = context.canvas.height
             context.clearRect (0.0, 0.0, w, h))
 
     /// Draws a rectangle with the given parameters.
-    let rect (x: float32, y: float32, width: float32, height: float32, fillStyle: string) : unit Draw =
+    let rect
+        (x: float32<'m>, y: float32<'m>, width: float32<'m>, height: float32<'m>, fillStyle: string)
+        : Draw<'m, unit> =
         fromAction (fun context ->
             context.fillStyle <- U3.Case1 fillStyle
             context.rect (float x, float y, float width, float height))
