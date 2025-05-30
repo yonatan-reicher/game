2b9e70eeadb8e5d8edbde6273b275ec243709552
Yonatan Reicher <yony252525@gmail.com>
2025-05-30 20:42:37 +0300

    Use Draw CE in Bar.draw

--- src/Draw.Bar.fs
+++ src/Draw.Bar.fs
@@ -2,7 +2,7 @@ namespace Draw
 
 
 open Rendering
-open FSharpPlus
+open Rendering.Draw.CE
 
 
 [<Struct>]
@@ -17,12 +17,7 @@ type Bar<[<Measure>] 'm> =
 [<RequireQualifiedAccess>]
 module Bar =
     let draw (bar: Bar<'m>) : Draw<'m, unit> =
-        Draw.rect (0f<_>, 0f<_>, bar.Width, bar.Height, bar.BackgroundColor)
-        >>= fun () ->
-            Draw.rect (0f<_>, 0f<_>, bar.Width * bar.Value, bar.Height, bar.Color)
-        // It seems that the `monad` builder is not supported with Fable...
-        // (It does not compile)
-        // monad {
-        //     do! Draw.rect (0f<_>, 0f<_>, bar.Width, bar.Height, bar.BackgroundColor)
-        //     do! Draw.rect (0f<_>, 0f<_>, bar.Width * bar.Value, bar.Height, bar.Color)
-        // }
+        draw {
+            do! Draw.rect (0f<_>, 0f<_>, bar.Width, bar.Height, bar.BackgroundColor)
+            do! Draw.rect (0f<_>, 0f<_>, bar.Width * bar.Value, bar.Height, bar.Color)
+        }
